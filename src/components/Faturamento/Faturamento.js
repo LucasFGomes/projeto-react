12ccddcea8b08e83511488b6cf320098ffc9eb84
LucasFGomes <lucasgomm33@gmail.com>
2019-09-12 01:05:17 -0300

    format files

--- src/components/Faturamento/Faturamento.js
+++ src/components/Faturamento/Faturamento.js
@@ -3,63 +3,60 @@ import { consultarFaturamento } from '../../services/faturamento';
 
 class Faturamento extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            detalhamento: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      detalhamento: []
+    };
+  }
 
-    componentDidMount() {
-        consultarFaturamento().then(dados => this.setState(dados));
-    }
+  componentDidMount() {
+    consultarFaturamento().then(dados => this.setState(dados));
+  }
 
-    render() {
-        return (
-            <>
-                <h2 className="mt-2">Faturamento</h2>
+  render() {
+    return (
+      <>
+        <h2 className="mt-2">Faturamento</h2>
 
-                <div className="row">
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-header">
-                                Total por forma de pagamento
-                            </div>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">Total por forma de pagamento </div>
 
-                            <table className="table">
-                            
-                                <thead>
-                                    <tr>
-                                        <th>Descrição</th>
-                                        <th className="text-right">Valor</th>
-                                    </tr>
-                                </thead>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Descrição</th>
+                    <th className="text-right">Valor</th>
+                  </tr>
+                </thead>
 
-                                <tbody>
+                <tbody>
 
-                                    {
-                                        this.state.detalhamento.map((item, indice) => {
-                                            return(
-                                                <tr key={indice}>
-                                                    <td>{item.descricao}</td>
-                                                    <td className="text-right">
-                                                        { item.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'}) }
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
+                  {
+                    this.state.detalhamento.map((item, indice) => {
+                      return (
+                        <tr key={indice}>
+                          <td>{item.descricao}</td>
+                          <td className="text-right">
+                            {item.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                          </td>
+                        </tr>
+                      )
+                    })
+                  }
 
-                                </tbody>
+                </tbody>
 
-                            </table>
+              </table>
 
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
-    }
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
 
 }
 
